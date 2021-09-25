264080550a7aef1886308eb9bbe15916352587d4
c8r.eth | Can O <54193214+c-8-r@users.noreply.github.com>
2021-09-25 03:34:40 +0300

    Create deploy.js

---
+++ contracts/deploy.js
@@ -0,0 +1,301 @@
+
+
+
+const assets = {
+    "heads": [
+        {
+            "name": "White Cartoon Head",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABUAAAAVAgMAAADUeU0FAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAAAAA////g93P0gAAAAN0Uk5TAP//RFDWIQAAAE1JREFUeJxjYAwNDQ1gYGBbtWrVFAYGKSC1FEKtdABTq9CpKAiVhY1CVSkGMQVk9EoGGMUKpJYwMDBC7GMEagTaDjQUqJCBQTQ0lIEBAOGFODXeEIGZAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Zombie Head",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABUAAAAVAgMAAADUeU0FAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAAAAAuOa2YggEuwAAAAN0Uk5TAP//RFDWIQAAAFNJREFUeJxFzrERgCAUg+GfQjeQwmkYwUIsGMF5XIHiZUqBxx3VV+QuCRw5A6HIUkPSB1ujOgb7QononE5Z2ISRLaIzy4JXh3cM8UgXPaypv8g3/ECQM85Y+NlUAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Beige Cartoon Head",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABUAAAAVAgMAAADUeU0FAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAAAAAsJ2IYbHolQAAAAN0Uk5TAP//RFDWIQAAAFVJREFUeJxFzrEVgDAIRdFHoRuYwmkYwcJYZATncYUUYUpDyDmpbvHhAxw5A1KsacfMPtg6NWiwL0xJwRk8QfGRCSNbpGCWSVTLOw75/oWHVf2LfMMPP0QzypQTC6IAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Cartoon Head",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAWAgMAAAC52oSoAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAlXdW////a+IkgwAAAAN0Uk5TAP//RFDWIQAAAFlJREFUeJxFzbEVgCAQA9BY6AZaMA0j0GDBCExDIRtoQab07gWf1/wiLzkAOcJuZXdOjgQslbwMko9H5AAOgxFBFFGdJNpHEUHZLMz69jM0feuf4cVm7DknvNNUOfQrDt8kAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Standard Head",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAWAgMAAAC52oSoAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAlXdWsJ2IiY4DxAAAAAN0Uk5TAP//RFDWIQAAAFpJREFUeJw9zbERgDAMA0BRwAZQZJqM4MYUGSHTpCAbQBFNiX0OuPlCJxmAZtit7M7JIcBSycsg+XhEDuAwmJGCElRHgvZRghTZLMz65mPBnL7/f15sxq4qeAHRtznfeXKHwAAAAABJRU5ErkJggg=="
+        }
+    ],
+    "hairs": [
+        {
+            "name": "Blonde Long Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABcAAAAWAgMAAABWGO+WAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAx4QYx4QYhibrMgAAAAN0Uk5TAP8AaVI5rAAAAFBJREFUeJxVzLENgDAMRNGfwhuQnlEYwUW8GTMgxuTsKJGonqW7M9AdaBExwERc9MSJhWLh1VWMiRxtNIVJ2xzCxg9fZEUPb6Hzmbz1hLP4ALJcE3JvR12uAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Brown Short Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABgAAAAEAQMAAACurW1bAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAIhcGck4K4gAAAAJ0Uk5TAP9bkSK1AAAAGUlEQVR4nGPg/3+AwY6hgqH+/x+G////AwAz3gf8kwxbPAAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Black Shitty Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAGAgMAAAC6DIYzAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAADAwNuOa2IlDxHAAAAAN0Uk5TAP//RFDWIQAAACxJREFUeJxjYA0NCWFgYBBdtTQ0bAJDKAgEMIQyBDCICgApBgbWAAYRBhAAAL/hByMiXx0lAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Red Curly Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAMAQMAAABcN9+FAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAA+R4eMTzzLAAAAAJ0Uk5TAP9bkSK1AAAALUlEQVR4nGPgMGBg4K/dwGB/fwtD3f8/DB8Y7gDxE4YDDDZQzAPGDgwcQMwAAHsBDXjUnYthAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Turquoise Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABUAAAAIAQMAAAAskSaQAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAHvmvMMjTOAAAAAJ0Uk5TAP9bkSK1AAAAKUlEQVR4nGOw/G/AYP//B8P/0z8Y/jH9YPjBUMDwgcGAIYFBgoGBQQIA5q0LcWHVnQ8AAAAASUVORK5CYII="
+        },
+        {
+            "name": "Blue Hat",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAGAQMAAAD9rPzjAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAEhynFAClxwAAAAJ0Uk5TAP9bkSK1AAAAGUlEQVR4nGNg+MHAIP//AMP//3/gmPF/AwCSOQ1Fs7ldRwAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Blonde Hedgehog Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAHAQMAAAA28C9GAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAj1oFfzCkIwAAAAJ0Uk5TAP9bkSK1AAAAJElEQVR4nGNgaGBgYGwRYDjcY8BQzPOAoZrnDEMFgwSDAoMEAFlGBg+CjWGEAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Purple Curly Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABcAAAAIAQMAAAAoZPatAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAqh75YagUHgAAAAJ0Uk5TAP9bkSK1AAAAJklEQVR4nGNgNjjAIP3zAMPd58cYviTUMSQwyDA8YOAAYgYgmwEAvCEJyPQKeR0AAAAASUVORK5CYII="
+        },
+        {
+            "name": "Blonde Short Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABQAAAAGAQMAAAD5WSzeAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAj1oFfzCkIwAAAAJ0Uk5TAP9bkSK1AAAAH0lEQVR4nGNgYWFgYP/PwGD/v4Gh/v8Dhh+MHxiAEABR6wj0WbOg5AAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Brown Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABMAAAAJAQMAAADq04VyAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAIhcGck4K4gAAAAJ0Uk5TAP9bkSK1AAAAKUlEQVR4nGNg/t/AUPz/AUM9EP8H4T8MDP8PMDDcb2BgmM/AwMDOwAAARaAO0Bf0JTMAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Purple Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABUAAAAHAQMAAADdx5RFAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAA/SP/i9DBoAAAAAJ0Uk5TAP9bkSK1AAAAI0lEQVR4nGNg/8/AUP//ARB/YHjAYMCQwCDB4MDAwXCAgQEAkm0IYzqva2UAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Green Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABAAAAAKAQMAAACHcEzfAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAHvkpNBP1LQAAAAJ0Uk5TAP9bkSK1AAAAI0lEQVR4nGOQZ2Cw/89g/5mhjoGhhoGhgIEhgYHhARgdYAAAZq0G2nIcDWUAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Blue Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABUAAAAGAQMAAAAWm0fgAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAOEPWO/tmwAAAAAJ0Uk5TAP9bkSK1AAAAIUlEQVR4nGNgqGBgsP/PwPD/fwND/f8PDP+YfzAcYCgAAGrNCcwrLW3iAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Yellow Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAIAQMAAADHpp2TAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAA+dgerJyD5AAAAAJ0Uk5TAP9bkSK1AAAAJklEQVR4nGNgcGBgsPufwCD//wuD5f4/DB8YZICYh+EAAwcQMwAAomwJdB6SrwUAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Activist Berfo Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAVBAMAAACwDgOmAAAAAXNSR0IB2cksfwAAAA9QTFRFAAAAijACXSAC7Dc3ijACxgSYjQAAAAV0Uk5TAP8A/wCPSv0yAAAAXklEQVR4nKXMyw2AMAwDUIcJ6qYTdAPkBUBi/5lIigocuOHTUz4GYMwUAAtdGZYeU11hhWsdN1KFwoWMNREE4tNcUDZYlFDI8Qj9sRF8u/5wn/0NszK93z6wfbndSp9engvrT7DeTgAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Purple Messy Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAGAQMAAAD9rPzjAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAA/SP/i9DBoAAAAAJ0Uk5TAP9bkSK1AAAAIElEQVR4nGOQs2BgsNOvYJCwrWBoYLBhWMDAw/CAgQUAMSwEVck5ouUAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Red Hedgehog Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAFAQMAAAB7OI5NAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAA/yMjBzGLMQAAAAJ0Uk5TAP9bkSK1AAAAHElEQVR4nGPgnWTAcGSSCoPLphyGFAYJBgUGBgA1CwRI/wKPdQAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Red Long Hair",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAABYAAAAVAgMAAAA/TvYGAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAXSACXSACR1XtCwAAAAN0Uk5TAP8AaVI5rAAAAElJREFUeJxjYBANDXVgYFANDQ0NWQDihIYmMIRCqQDXwNAAIMXqIAqkGBgZWEEUA4xihFNhhKkoiL4AJCqBoQFELQBTbCACRAEAA28Ut4pCEtEAAAAASUVORK5CYII="
+        }
+    ],
+    "eyes": [
+        {
+            "name": "Blue Pirate Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA4AAAAHAgMAAABFKZSKAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAAAAAJlncW7+rcAAAAAN0Uk5TAP//RFDWIQAAACBJREFUeJxjYGBhAAIBIGZ0ABKsAQwLgATTAgZBEBeEASclAtNZEhFrAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Green Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA0AAAADAgMAAAA1j22fAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAMWAcgfZMEsOYlQAAAAN0Uk5TAP//RFDWIQAAABRJREFUeJxjEGFgZWDIZJByYACxAAekAPbuYo+wAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "S eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA4AAAAFAQMAAABPQU9RAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAJlncfbv7fwAAAAJ0Uk5TAP9bkSK1AAAAFklEQVR4nGOwYGAo/sHw2YahHsgqAAAexwP69QlmtgAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Black Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAsAAAACAQMAAAC0bbStAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAAAAApWe5zwAAAAJ0Uk5T/wDltzBKAAAADklEQVR4nGOoT2CQZwAABD8A/xOvsBUAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Melancholic Blue Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA0AAAACAQMAAAC5c8TqAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAFXzx9rosJAAAAAJ0Uk5TAP9bkSK1AAAADklEQVR4nGMwSGD4UAEABM4B+XNPxZkAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Purple Cool Glasses",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA0AAAAEAQMAAABvKif3AAAAAXNSR0IB2cksfwAAAAZQTFRFAAAA9w/C5x5jyQAAAAJ0Uk5TAP9bkSK1AAAAFElEQVR4nGNIMGD4/4OhoIBBQQEAGRwDqBWRxdMAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Angry Black Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAsAAAACAQMAAAC0bbStAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAAAAApWe5zwAAAAJ0Uk5T/wDltzBKAAAADklEQVR4nGOwb2CQZwAAA38A357kA/oAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Red Glasses",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA0AAAAEAQMAAABvKif3AAAAAXNSR0IB2cksfwAAAAZQTFRF9w8kAAAAAPA/1gAAAAJ0Uk5T/wDltzBKAAAAFElEQVR4nGNgYGBgZ2DoP8Ew/wQACDUCxsqf21IAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Purple Glasses",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA4AAAAEAQMAAACEHZz0AAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAyDXQHjluVQAAAAJ0Uk5TAP9bkSK1AAAAFElEQVR4nGMoL2C4/4fhQwbDAQsAH20FE93rlS4AAAAASUVORK5CYII="
+        },
+        {
+            "name": "Green Thin Glasses",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA0AAAADAQMAAAByLxdPAAAAAXNSR0IB2cksfwAAAAZQTFRFRfcPAAAAWIW+CwAAAAJ0Uk5T/wDltzBKAAAAD0lEQVR4nGOQP8EABPIMAAa3AQepankzAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Snake Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAsAAAAEAQMAAABiNFewAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAAAAAApWe5zwAAAAJ0Uk5T/wDltzBKAAAAFElEQVR4nGOQZ2DoU2A4l8BwPwEADlUDOz75AcUAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Red Irregular Glasses",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA4AAAAFAQMAAABPQU9RAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAA9w8krnckhAAAAAJ0Uk5TAP9bkSK1AAAAF0lEQVR4nGOws2F4psJg84SBwYaBQQIAGu0C+c6CXesAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Curious Blue Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA0AAAAFAgMAAADj1o6CAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAIz/S////rcnmDwAAAAN0Uk5TAP//RFDWIQAAABxJREFUeJxjYGFgYWAQBUKGSIdQByAtycAAEgMAFCMBlx4V6CAAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Red Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAsAAAADAQMAAAB/MWcIAAAAAXNSR0IB2cksfwAAAAZQTFRF/yIiAAAA8JkJ0gAAAAJ0Uk5T/wDltzBKAAAAEUlEQVR4nGOwb2CoP8Bg3wAAC/oCvgKDZ9oAAAAASUVORK5CYII="
+        },
+        {
+            "name": "Flaring Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA0AAAADAgMAAAA1j22fAAAAAXNSR0IB2cksfwAAAAlQTFRF/yIi6/4dAAAAAKkiKgAAAAN0Uk5T//8A18oNQQAAABRJREFUeJxjEFu1lIEBTHCtWsEAAB+wBCcmiQYqAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Yellow Bee Eyes",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAA0AAAAEAQMAAABvKif3AAAAAXNSR0IB2cksfwAAAAZQTFRF/6kRAAAA1DHfHgAAAAJ0Uk5T/wDltzBKAAAAFElEQVR4nGPgb2D4/4Nh/gmG/g4AHFIFBQlwGwcAAAAASUVORK5CYII="
+        }
+    ],
+    "mouths": [
+        {
+            "name": "Mouth With Lipstick",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAcAAAADAQMAAABlDYeGAAAAAXNSR0IB2cksfwAAAAZQTFRFAAAA5SUlqbmvJAAAAAJ0Uk5TAP9bkSK1AAAADklEQVR4nGOwYDjGYAEAA6gBNwTb1M0AAAAASUVORK5CYII="
+        },
+        {
+            "name": "Tongue Left Down",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAcAAAADAgMAAAAirf1WAAAAAXNSR0IB2cksfwAAAAlQTFRFGhoa/wUFAAAAj6IbwgAAAAN0Uk5T//8A18oNQQAAABFJREFUeJxjYGBgiFrBkLUCAAXnAhUP4lmqAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Cool Beard",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAGAgMAAAByYGzlAAAAAXNSR0IB2cksfwAAAAlQTFRFCwoKAAAA/z4+CroGoAAAAAN0Uk5T/wD/aewfvgAAABpJREFUeJxjEBBgcGBhyFrCEBrC4MjC4OoCABfjAupfWKCsAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Orange Beard",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFAgMAAADqPS74AAAAAXNSR0IB2cksfwAAAAlQTFRFAAAAk0EUmJmO+/pKIAAAAAN0Uk5TAP//RFDWIQAAABpJREFUeJxjEA1lYAgNdWAQW8rAAGKzhjAAAB8qAtNpkU+8AAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Triangle Beard",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAGAgMAAACHnudVAAAAAXNSR0IB2cksfwAAAAxQTFRFRyscAAAA////mJmOQBsckQAAAAR0Uk5T/wD//9OwcpQAAAAaSURBVHicY2CAAqcdAgwBCwIYQhgDGEJZAwAgkQOaJZCuBgAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Standard Mouth",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAcAAAACAQMAAACuUVQjAAAAAXNSR0IB2cksfwAAAAZQTFRFGhoaAAAAEt6g6gAAAAJ0Uk5T/wDltzBKAAAADElEQVR4nGNgYOADAAASAA8HhlLMAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Tricky Mouth",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAGAgMAAAByYGzlAAAAAXNSR0IB2cksfwAAAAlQTFRF/z4+AAAACwoKtFiUvgAAAAN0Uk5T/wD/aewfvgAAABpJREFUeJxjEA1gcGBhCA1hyFrCEDWFISwEAB34A/nQ7XZ6AAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Small Mouth With Mustache",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAUAAAADAgMAAAAmWC1rAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAADg4O81RU49WB1gAAAAN0Uk5TAP//RFDWIQAAABFJREFUeJxjEGVgCHVg0GIAAAOuANU7ibUdAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Leftist Mustache",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAsAAAAEAgMAAAAllC1gAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAACwoK81RUyfoh8gAAAAN0Uk5TAP//RFDWIQAAABZJREFUeJxjYA0NYBAF4rBVIQwBDCIAGRgDHRYKqF8AAAAASUVORK5CYII="
+        },
+        {
+            "name": "Blower Mouth",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAEAgMAAAA7XR3TAAAAAXNSR0IB2cksfwAAAAxQTFRFAAAA5SUlysrKt7e3Xe28NAAAAAR0Uk5TAP///7MtQIgAAAAUSURBVHicYxBlYMhzYKh2YBBlAAAJtAGUReZSpgAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Chad Mouth",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAkAAAAEAgMAAAAhYf1dAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAACwoK3Nzc6tI5fgAAAAN0Uk5TAP//RFDWIQAAABhJREFUeJxjYGBhYAgMZWAIXcnAwBrqAAAOVwJDSoAD1QAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Cute Mouth",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAMAAAACAQMAAACnuvRZAAAAAXNSR0IB2cksfwAAAAZQTFRFCwsLAAAAb3H/HAAAAAJ0Uk5T/wDltzBKAAAADElEQVR4nGNIYGAAAAEkAGECfVgzAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Horny Mouth",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAgAAAADAgMAAADTpqbbAAAAAXNSR0IB2cksfwAAAAlQTFRFpTtCmJmOAAAABsYVXwAAAAN0Uk5T//8A18oNQQAAABFJREFUeJxjYGBgYA1gaGACAAJkANhGMXIkAAAAAElFTkSuQmCC"
+        },
+        {
+            "name": "Tongue Down",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAgAAAAEAgMAAADOo5ZjAAAAAXNSR0IB2cksfwAAAAlQTFRFCwoKAAAA/yMjltAGZQAAAAN0Uk5T/wD/aewfvgAAABJJREFUeJxjYGBgCGplCJsKRAALggKutxY/IwAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Demonic Beard",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAIAgMAAABIag2VAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAA/z4+CwoKfy0lDAAAAAN0Uk5TAP//RFDWIQAAABtJREFUeJxjYHVgCA1hiJoCIh1YGBxZQCSrAwAxtQOYVqugGAAAAABJRU5ErkJggg=="
+        },
+        {
+            "name": "Sad Mouth",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAkAAAADAQMAAAB7xLc1AAAAAXNSR0IB2cksfwAAAAZQTFRFAAAACwoKswhXugAAAAJ0Uk5TAP9bkSK1AAAAEUlEQVR4nGPQZ2DY2cBwgAEACJ4CKUH5pX4AAAAASUVORK5CYII="
+        },
+        {
+            "name": "Mop Specialist Mouth",
+            "data": "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAFAgMAAAD09B5LAAAAAXNSR0IB2cksfwAAAAlQTFRFAAAARysc9hlH/xFXbwAAAAN0Uk5TAP//RFDWIQAAABVJREFUeJxjEA1gCA1hyFrC4MACRAAV1AKljE/q8AAAAABJRU5ErkJggg=="
+        }
+    ]
+}
+
+
+const deployScript = async ({ abiFile }) => {
+
+    const metadata = JSON.parse(await remix.call('fileManager', 'getFile', `${abiFile}`))
+    const accounts = await web3.eth.getAccounts()
+
+    let contract = new web3.eth.Contract(metadata.abi)
+
+
+
+    contract = contract.deploy({
+        data: metadata.data.bytecode.object,
+    })
+
+    newContractInstance = await contract.send({
+        from: accounts[0]
+    })
+    console.log("deployed: ", newContractInstance.options.address)
+    return newContractInstance
+}
+
+(async () => {
+
+
+    const accounts = await web3.eth.getAccounts()
+
+    try {
+
+        console.log('deploying contract...')
+        let turkPunksContract = await deployScript({ abiFile: "browser/contracts/artifacts/TurkPunks.json" })
+
+        console.log(`pushing to storage...`)
+
+        for (const head of assets.heads) {
+            console.log("deploying head...")
+             turkPunksContract.methods.uploadHead(head).send({ from: accounts[0] })
+        }
+
+        for (const eye of assets.eyes) {
+            console.log("deploying eye...")
+             turkPunksContract.methods.uploadEye(eye).send({ from: accounts[0] })
+        }
+
+        for (const hair of assets.hairs) {
+            console.log("deploying hair...")
+             turkPunksContract.methods.uploadHair(hair).send({ from: accounts[0] })
+        }
+
+
+        for (const mouth of assets.mouths) {
+            console.log("deploying mouth...")
+             turkPunksContract.methods.uploadMouth(mouth).send({ from: accounts[0] })
+        }
+
+
+
+        console.log(turkPunksContract.options.address)
+
+
+    } catch (e) {
+        console.log(e.message)
+    }
+})()
