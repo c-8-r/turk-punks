c843aed2c0ee9eb22239ccb04bed8830db7bd4a5
c8r <54193214+c-8-r@users.noreply.github.com>
2021-09-30 00:15:34 +0300

    Update TurkPunks.sol

--- contracts/TurkPunks.sol
+++ contracts/TurkPunks.sol
@@ -1,223 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract TurkPunks is ERC721, ReentrancyGuard, Ownable {
-   
-    struct Asset {
-        string name;
-        string data;
-    }
-
-    Asset[] internal heads;
-    Asset[] internal hairs;
-    Asset[] internal eyes;
-    Asset[] internal mouths;
-
-    uint256 public mintCount;
-    uint256 public MAX_SUPPLY = 11000;
-    uint256 public MINT_PRICE = 0.03 ether;
+contract TurkPunks is ERC721URIStorage, Ownable, ReentrancyGuard {
+    using Counters for Counters.Counter;
+    
     bool started = false;
+    Counters.Counter public _tokenIdCounter;
+    uint256 public MAX_SUPPLY = 0;
+    string baseURI;
+    uint256[] internal orderArray;
 
-    constructor() ERC721("TurkPunks", "TP") {}
+
+
+    // INTERNAL VIEWS
     
-    function withdrawToPayees(uint256 _amount) internal {
-        uint256 amount = _amount;
-        payable(0x3B99E794378bD057F3AD7aEA9206fB6C01f3Ee60).transfer(
-            amount / 3
-        ); // artist
-
-        payable(0x575CBC1D88c266B18f1BB221C1a1a79A55A3d3BE).transfer(
-            amount / 3
-        ); // developer
-
-        payable(0xBF7288346588897afdae38288fff58d2e27dd235).transfer(
-            amount / 3
-        ); // developer
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
     }
     
-    
-    function _mint(address _to) internal {
-        require(mintCount < MAX_SUPPLY, "Sold Out!");
-        _safeMint(_to, mintCount);
-        mintCount++;
-    }
-    
-    
-
-    
-    function devMint(address _to) external onlyOwner {
-        _mint(_to);
-    }
-
-    function startWithPreMint(uint256 _amount) external onlyOwner {
-        require(started == false);
-        for (uint256 i; i < _amount; i++) {
-            _mint(msg.sender);
-        }
-        started = true;
-    }
-
-    function mint(uint256 _amount) external payable nonReentrant {
-        require(started == true, "not started");
-        require(
-            msg.value >= _amount * MINT_PRICE,
-            "please send 0.03 ether to mint."
-        );
-        withdrawToPayees(msg.value);
-        for (uint256 i; i < _amount; i++) {
-            _mint(msg.sender);
-        }
-    }
-
-
-
-    function uploadHead(Asset calldata _head) external {
-        heads.push(_head);
-    }
-
-    function uploadHair(Asset calldata _hair) external {
-        hairs.push(_hair);
-    }
-
-    function uploadEye(Asset calldata _eye) external {
-        eyes.push(_eye);
-    }
-
-    function uploadMouth(Asset calldata _mouth) external {
-        mouths.push(_mouth);
-    }
-
-    function renderRarity(uint8 rarity) internal pure returns (string memory) {
-        if (rarity == 1) {
-            // not rare
-            return "";
-        } else if (rarity == 2) {
-            // rare
-            return "<text y='30' x='20' class='vr'>R</text>";
-        } else if (rarity == 3) {
-            // very rare
-            return
-                "<text y='30' x='0' class='vr'>V</text><text y='30' x='20' class='vr'>R</text>";
-        } else {
-            return "";
-        }
-    }
-
-    function renderAssetInSvg(
-        uint8 x,
-        uint8 y,
-        string memory _base64
-    ) internal pure returns (string memory) {
-        return
-            string(
-                abi.encodePacked(
-                    "<image x='",
-                    toString(x),
-                    "' y='",
-                    toString(y),
-                    "' href='data:image/png;base64,",
-                    _base64,
-                    "' />"
-                )
-            );
-    }
-
-    function render(
-        uint8 _rarity,
-        string memory _hair_b64,
-        string memory _eye_b64,
-        string memory _head_b64,
-        string memory _mouth_b64
-    ) internal pure returns (string memory) {
-        return
-            string(
-                abi.encodePacked(
-                    "<svg xmlns='http://www.w3.org/2000/svg' height='150' width='150' viewBox='0 0 150 150'><style>.vr{ font-size: 5px; fill: red; }</style><rect width='100%' height='100%' fill='white' /><svg viewBox='-7.5 -7.5 40 40'>",
-                    renderRarity(_rarity),
-                    renderAssetInSvg(0, 1, _head_b64),
-                    renderAssetInSvg(0, 0, _hair_b64),
-                    renderAssetInSvg(3, 5, _eye_b64),
-                    renderAssetInSvg(7, 15, _mouth_b64),
-                    "</svg></svg>"
-                )
-            );
-    }
-
-     function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721)
-        returns (string memory)
-    {
-        super.tokenURI(tokenId);
-        uint256 count = 0;
-        for (uint8 headIndex; headIndex < heads.length; headIndex++) {
-            for (uint8 hairIndex; hairIndex < hairs.length; hairIndex++) {
-                for (uint8 eyeIndex; eyeIndex < eyes.length; eyeIndex++) {
-                    for (
-                        uint8 mouthIndex;
-                        mouthIndex < mouths.length;
-                        mouthIndex++
-                    ) {
-                        if (count == tokenId) {
-                            return
-                                string(
-                                    abi.encodePacked(
-                                        "data:application/json;base64,",
-                                        encode(
-                                            bytes(
-                                                abi.encodePacked(
-                                                    '{"name": "TurkPunk #',
-                                                    toString(tokenId),
-                                                    '", "description": "tp 100% ipfs.", "image": "data:image/svg+xml;base64,',
-                                                    string(
-                                                        encode(
-                                                            abi.encodePacked(
-                                                                render(
-                                                                    2,
-                                                                    hairs[
-                                                                        hairIndex
-                                                                    ].data,
-                                                                    eyes[
-                                                                        eyeIndex
-                                                                    ].data,
-                                                                    heads[
-                                                                        headIndex
-                                                                    ].data,
-                                                                    mouths[
-                                                                        mouthIndex
-                                                                    ].data
-                                                                )
-                                                            )
-                                                        )
-                                                    ),
-                                                    '", "attributes": [{"trait_type": "Head", "value": "',
-                                                    heads[headIndex].name,
-                                                    '"}, {"trait_type": "Eyes", "value": "',
-                                                    eyes[eyeIndex].name,
-                                                    '"}, {"trait_type": "Hair", "value": "',
-                                                    hairs[hairIndex].name,
-                                                    '"}, {"trait_type": "Mouth", "value": "',
-                                                    mouths[mouthIndex].name,
-                                                    '"} ]  }'
-                                                )
-                                            )
-                                        ),
-                                        "#"
-                                    )
-                                );
-                        }
-                        count++;
-                    }
-                }
-            }
-        }
-        return "";
-    }
-
+  
     /**
      * @dev Converts a `uint256` to its ASCII `string` decimal representation.
      */
@@ -241,83 +47,99 @@ contract TurkPunks is ERC721, ReentrancyGuard, Ownable {
         }
         return string(buffer);
     }
-
-    // @title Base64
-    // @author Brecht Devos - <brecht@loopring.org>
-    // @notice Provides a function for encoding some bytes in base64
-    function encode(bytes memory data) internal pure returns (string memory) {
-        string
-            memory TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-        if (data.length == 0) return "";
-
-        // load the table into memory
-        string memory table = TABLE;
-
-        // multiply by 4/3 rounded up
-        uint256 encodedLen = 4 * ((data.length + 2) / 3);
-
-        // add some extra buffer at the end required for the writing
-        string memory result = new string(encodedLen + 32);
-
-        assembly {
-            // set the actual output length
-            mstore(result, encodedLen)
-
-            // prepare the lookup table
-            let tablePtr := add(table, 1)
-
-            // input ptr
-            let dataPtr := data
-            let endPtr := add(dataPtr, mload(data))
-
-            // result ptr, jump over length
-            let resultPtr := add(result, 32)
-
-            // run over the input, 3 bytes at a time
-            for {
-
-            } lt(dataPtr, endPtr) {
-
-            } {
-                dataPtr := add(dataPtr, 3)
-
-                // read 3 bytes
-                let input := mload(dataPtr)
-
-                // write 4 characters
-                mstore(
-                    resultPtr,
-                    shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F))))
-                )
-                resultPtr := add(resultPtr, 1)
-                mstore(
-                    resultPtr,
-                    shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F))))
-                )
-                resultPtr := add(resultPtr, 1)
-                mstore(
-                    resultPtr,
-                    shl(248, mload(add(tablePtr, and(shr(6, input), 0x3F))))
-                )
-                resultPtr := add(resultPtr, 1)
-                mstore(
-                    resultPtr,
-                    shl(248, mload(add(tablePtr, and(input, 0x3F))))
-                )
-                resultPtr := add(resultPtr, 1)
-            }
-
-            // padding with '='
-            switch mod(mload(data), 3)
-            case 1 {
-                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
-            }
-            case 2 {
-                mstore(sub(resultPtr, 1), shl(248, 0x3d))
-            }
+    
+    // INTERNAL ACTIONS
+    
+    
+    
+    function incrementOrderArray(uint256[] calldata supply) public onlyOwner {
+        for(uint256 i; i < supply.length; i++) {
+            orderArray.push(supply[i]);
         }
-
-        return result;
+        MAX_SUPPLY = orderArray.length;
     }
+    
+    
+    function withdrawToPayees(uint256 _amount) internal {
+        uint256 amount = _amount;
+        payable(0x3B99E794378bD057F3AD7aEA9206fB6C01f3Ee60).transfer(
+            (amount / 100) * 40
+        ); // artist
+
+        payable(0x575CBC1D88c266B18f1BB221C1a1a79A55A3d3BE).transfer(
+            (amount / 100) * 40
+        ); // developer
+
+        payable(0x50D80101e43db03740ad27F2aD6bC919012dc1f9).transfer(
+            (amount / 100) * 20
+        ); // autism
+    }
+    
+    
+    function _mint(address _to) internal {
+        require(_tokenIdCounter.current() < MAX_SUPPLY, "Sold Out!");
+        _safeMint(_to, orderArray[_tokenIdCounter.current()]);
+        _tokenIdCounter.increment();
+    }
+
+    
+    // EXTERNAL VIEWS
+    
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721URIStorage)
+        returns (string memory)
+    {
+        require(_exists(tokenId), "not exists");
+        return string(abi.encodePacked(_baseURI(), toString(tokenId), ".json"));
+    }
+    
+    
+    // EXTERNAL ACTIONS
+    function mint(uint256 _amount) external payable nonReentrant {
+        require(started == true, "not started");
+        require(
+            msg.value >= _amount * 0.03 ether,
+            "please send 0.03 ether to mint."
+        );
+        require( _tokenIdCounter.current() + _amount <= MAX_SUPPLY, "exceeds the max supply");
+        withdrawToPayees(msg.value);
+        for (uint256 i; i < _amount; i++) {
+            _mint(msg.sender);
+        }
+    }
+
+    
+    // EXTERNAL OWNER ONLY!
+    
+    function setBaseURI(string calldata _uri) external onlyOwner {
+        baseURI = _uri;
+    }
+    
+     // to convert onchain
+    function setTokenURI(uint256 _tokenId, string calldata _tokenURI)
+        external
+        onlyOwner
+    {
+        _setTokenURI(_tokenId, _tokenURI);
+    }
+    
+    function devMint(address _to) external onlyOwner {
+        _mint(_to);
+    }
+
+    function startWithPreMint(uint256 _amount) external onlyOwner {
+        require(started == false);
+        for (uint256 i; i < _amount; i++) {
+            _mint(msg.sender);
+        }
+        started = true;
+    }
+    
+    constructor(string memory _uri) ERC721("TurkPunks", "TP") onlyOwner { 
+        baseURI = _uri;
+    }
+    
+    
 }
